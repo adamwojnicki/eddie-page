26ff2ed5397aaf5d47a3ef84107c502e2e22e9e5
Adam Wojnicki <wojnicki.ada1@gmial.com>
2020-07-29 23:02:12 +0200

    Interactive menu and header bugfixes

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const handleMobileMenu = (e, icon) => {
-  e.preventDefault();
+const handleMobileMenu = (icon) => {
   const nav = document.querySelector(".navigation__list");
   nav.classList.toggle("active");
   document.body.classList.toggle("menu-active");
   const iconChange = nav.classList.contains("active") ? "close" : "menu";
   icon.childNodes[0].textContent = iconChange;
-  const closeNavAfterMenuClick = () => {
-    document.body.classList.remove("menu-active");
-    nav.classList.remove("active");
-  };
-  const menuItems = document.querySelectorAll(".navigation__item");
-  menuItems.forEach((item) =>
-    item.addEventListener("click", closeNavAfterMenuClick())
-  );
+  const menuItems = document.querySelectorAll(".navigation__item a");
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => closeNavAfterMenuClick(nav, icon));
+  });
+};
+
+const closeNavAfterMenuClick = (nav, icon) => {
+  document.body.classList.remove("menu-active");
+  nav.classList.remove("active");
+  icon.childNodes[0].textContent = "menu";
+  console.log(icon);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   const mobileMenuIcon = document.querySelector(".hamburger");
-  mobileMenuIcon.addEventListener("click", (e) =>
-    handleMobileMenu(e, mobileMenuIcon)
+  mobileMenuIcon.addEventListener("click", () =>
+    handleMobileMenu(mobileMenuIcon)
   );
 });
